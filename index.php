d9e70fc8afb63459139dabdf7a577a6be5ac1fb5
Nazadain <oldfather3@mail.ru>
2024-04-29 21:21:19 +0500

    createDocument

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
-// require $_SERVER["DOCUMENT_ROOT"].'/phpword/autoload.php';
-// $phpWord = new \PhpOffice\PhpWord\PhpWord(); 
-// $_doc = new \PhpOffice\PhpWord\TemplateProcessor('document.docx');
-phpinfo();
+include $_SERVER["DOCUMENT_ROOT"].'/vendor/autoload.php';
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
+$_doc = new \PhpOffice\PhpWord\TemplateProcessor('local-template.docx');
+
+$_doc->setValue('course', "2"); 
+$_doc->setValue('group', "1121б"); 
+$_doc->setValue('fullName', "Пономаренко Егор Андреевич"); 
+
+$img_Dir = $_SERVER['DOCUMENT_ROOT']."/"; 
+@mkdir($img_Dir, 0777);
+$file = str_replace("/","-", "Договор №".date("d-m-Y")).".docx";
+$_doc->saveAs($img_Dir.$file);
