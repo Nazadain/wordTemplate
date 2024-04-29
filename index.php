2e7b0e12474a2c629e465c104cb29dffbbcecd0d
Nazadain <oldfather3@mail.ru>
2024-04-29 15:19:26 +0500

    Необходимые файлы
    Все необходимые файлы для старта проекта

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+// require $_SERVER["DOCUMENT_ROOT"].'/phpword/autoload.php';
+// $phpWord = new \PhpOffice\PhpWord\PhpWord(); 
+// $_doc = new \PhpOffice\PhpWord\TemplateProcessor('document.docx');
+phpinfo();
